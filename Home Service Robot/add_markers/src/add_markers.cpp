e09734d51107586880d3bbc3b2116faa5771f752
feng <545907361@qq.com>
2020-06-03 04:30:29 +0800

    修改pick_objects 订阅mark话题使robo能自主寻找mark

--- Home Service Robot/add_markers/src/add_markers.cpp
+++ Home Service Robot/add_markers/src/add_markers.cpp
@@ -54,13 +54,13 @@ int main( int argc, char** argv )
   marker.pose.orientation.w = 1.0;
 
   // Set the scale of the marker -- 1x1x1 here means 1m on a side
-  marker.scale.x = 0.2;
-  marker.scale.y = 0.2;
-  marker.scale.z = 0.2;
+  marker.scale.x = 0.5;
+  marker.scale.y = 0.5;
+  marker.scale.z = 0.5;
 
   // Set the color -- be sure to set alpha to something non-zero!
-  marker.color.r = 0.0f;
-  marker.color.g = 1.0f;
+  marker.color.r = 1.0f;
+  marker.color.g = 0.0f;
   marker.color.b = 0.0f;
   marker.color.a = 1.0;
 
@@ -97,17 +97,17 @@ int main( int argc, char** argv )
   goal_x = marker.pose.position.x;
   goal_y = marker.pose.position.y;
   
-  ROS_INFO("Delivering object");
-  
+  marker.action = visualization_msgs::Marker::ADD;
+  marker_pub.publish(marker);
+  ROS_INFO("Object has been add");
   while (ros::ok() && !near_goal) {
     ros::spinOnce();
     time_between_ros_wakeups.sleep();
   }
 
-  ROS_INFO("Object has been delivered");
-  marker.action = visualization_msgs::Marker::ADD;
+  ROS_INFO("Object has been picked up");
+  marker.action = visualization_msgs::Marker::DELETE;
   marker_pub.publish(marker);
-  
   sleep(5);
   
   r.sleep();
--- Home Service Robot/pick_objects/src/pick_objects.cpp
+++ Home Service Robot/pick_objects/src/pick_objects.cpp
@@ -1,15 +1,14 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
-
+#include <visualization_msgs/Marker.h>
+float goal_x = 0.0;
+float goal_y = 0.0;
 // Define a client for to send goal requests to the move_base server through a SimpleActionClient
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
-int main(int argc, char** argv){
-  // Initialize the simple_navigation_goals node
-  ros::init(argc, argv, "pick_objects");
-
-  //tell the action client that we want to spin a thread by default
+void goalmakr_callback(const visualization_msgs::Marker::ConstPtr& goalmakr_msg)
+{
   MoveBaseClient ac("move_base", true);
 
   // Wait 5 sec for move_base action server to come up
@@ -22,10 +21,55 @@ int main(int argc, char** argv){
   // set up the frame parameters
   goal.target_pose.header.frame_id = "/map";
   goal.target_pose.header.stamp = ros::Time::now();
-
+  goal_x = goalmakr_msg->pose.position.x;
+  goal_y = goalmakr_msg->pose.position.y;
   // Define a position and orientation for the robot to reach
-  goal.target_pose.pose.position.x = 2.5;
-  goal.target_pose.pose.position.y = -0.5;
+  goal.target_pose.pose.position.x = goal_x;
+  goal.target_pose.pose.position.y = goal_y;
+  goal.target_pose.pose.orientation.w = 1.0;
+
+   // Send the goal position and orientation for the robot to reach
+  ROS_INFO("Sending  goal");
+  ac.sendGoal(goal);
+
+  // Wait an infinite time for the results
+  ac.waitForResult();
+  
+  // Check if the robot reached its goal
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    ROS_INFO("Hooray, the base moved to the 1st goal");
+  else
+    ROS_INFO("The base failed to move to the 1st goal for some reason");
+  
+  ros::Duration(5).sleep();
+}
+int main(int argc, char** argv){
+  // Initialize the simple_navigation_goals node
+  ros::init(argc, argv, "pick_objects");
+  ros::NodeHandle n;
+  //ROS_INFO("运行过了spin");
+  /*
+  //tell the action client that we want to spin a thread by default
+  MoveBaseClient ac("move_base", true);
+
+  // Wait 5 sec for move_base action server to come up
+  while(!ac.waitForServer(ros::Duration(5.0))){
+    ROS_INFO("Waiting for the move_base action server to come up");
+  }
+
+  //move_base_msgs::MoveBaseGoal goal;
+
+  // set up the frame parameters
+  goal.target_pose.header.frame_id = "/map";
+  goal.target_pose.header.stamp = ros::Time::now();
+  */
+
+  ros::Subscriber marker_sub = n.subscribe("visualization_marker", 1000, goalmakr_callback);
+  ros::spin();
+  /*
+  // Define a position and orientation for the robot to reach
+  goal.target_pose.pose.position.x = goal_x;
+  goal.target_pose.pose.position.y = goal_y;
   goal.target_pose.pose.orientation.w = 1.0;
 
    // Send the goal position and orientation for the robot to reach
@@ -42,7 +86,8 @@ int main(int argc, char** argv){
     ROS_INFO("The base failed to move to the 1st goal for some reason");
   
   ros::Duration(5).sleep();
-  
+  */
+  /*
   // Define a position and orientation for the robot to reach
   goal.target_pose.pose.position.x = -4.0;
   goal.target_pose.pose.position.y = 5.0;
@@ -60,6 +105,7 @@ int main(int argc, char** argv){
     ROS_INFO("Hooray, the base moved to the 2nd goal");
   else
     ROS_INFO("The base failed to moveto the 2nd goal for some reason");
-
+  */
+  
   return 0;
 }
